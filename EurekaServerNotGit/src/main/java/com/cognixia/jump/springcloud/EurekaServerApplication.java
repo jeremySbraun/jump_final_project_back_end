66a9695756ec1ab3786f2fbd7244acc5b4b0d7a3
Jeremy Braun <85400145+jeremySbraun@users.noreply.github.com>
2021-09-07 14:56:19 -0500

    TestingEureka

--- EurekaServerNotGit/src/main/java/com/cognixia/jump/springcloud/EurekaServerApplication.java
+++ EurekaServerNotGit/src/main/java/com/cognixia/jump/springcloud/EurekaServerApplication.java
@@ -1,11 +1,14 @@
-package com.cognixia.jump.finalproject;
+package com.cognixia.jump.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableEurekaServer
+
+
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
